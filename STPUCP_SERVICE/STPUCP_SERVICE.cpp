da7b3775954e29408eab43df6931e206f6e17db5
Renzo <Renzo@LAPTOP-CNOL1GOG>
2024-04-25 21:01:48 -0500

    RAMA 4 _ REVISAR

--- STPUCP_SERVICE/STPUCP_SERVICE.cpp
+++ STPUCP_SERVICE/STPUCP_SERVICE.cpp
@@ -1,4 +1,20 @@
 #include "pch.h"
-
 #include "STPUCP_SERVICE.h"
 
+int STPUCP_SERVICE::Service::AddUser(Usuario^)
+{
+	return 0;
+}
+
+int STPUCP_SERVICE::Service::UpdateUser(Usuario^)
+{
+	return 0;
+}
+
+
+
+Usuario^ STPUCP_SERVICE::Service::(int Id)
+{
+	throw gcnew System::NotImplementedException();
+	// TODO: Insertar una instrucción "return" aquí
+}
--- STPUCP_SERVICE/STPUCP_SERVICE.h
+++ STPUCP_SERVICE/STPUCP_SERVICE.h
@@ -1,10 +1,21 @@
 #pragma once
+#include "../STPUCP_Model/Usuario.h"
 
 using namespace System;
+using namespace STPUCP_Model;
+using namespace System::Collections::Generic;
 
-namespace STPUCPSERVICE {
-	public ref class Class1
+namespace STPUCP_SERVICE {
+	public ref class Service
 	{
-		// TODO: Agregue aquí los métodos de esta clase.
+	private:
+		
+		static List<Usuario^>^ User = gcnew List<Usuario^>();
+	public:
+		static int AddUser(Usuario^);
+		static int UpdateUser(Usuario^);
+		
+		static List<Usuario^>^ GeneralUsers();
+		static Usuario^ IdUsers(int Id);
 	};
 }
