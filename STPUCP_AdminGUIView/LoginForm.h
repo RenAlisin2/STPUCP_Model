796d9351c39e146fb43faf94c67ec80518b4ccc5
HP <HP@DESKTOP-Q59QJKB>
2024-05-02 17:18:14 -0500

    NUEVAS INTERFACES

---
+++ STPUCP_AdminGUIView/LoginForm.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include "RegistroForm.h"
+
+namespace STPUCPAdminGUIView {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de LoginForm
+	/// </summary>
+	public ref class LoginForm : public System::Windows::Forms::Form
+	{
+	public:
+		LoginForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~LoginForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ txtUsuario;
+	private: System::Windows::Forms::TextBox^ txtContraseña;
+	private: System::Windows::Forms::Button^ btnIngresar;
+	private: System::Windows::Forms::Button^ btnRegistrarse;
+	private: System::Windows::Forms::Label^ label3;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->txtUsuario = (gcnew System::Windows::Forms::TextBox());
+			this->txtContraseña = (gcnew System::Windows::Forms::TextBox());
+			this->btnIngresar = (gcnew System::Windows::Forms::Button());
+			this->btnRegistrarse = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(65, 37);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(75, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Código PUCP:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(65, 70);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(61, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Contraseña";
+			// 
+			// txtUsuario
+			// 
+			this->txtUsuario->Location = System::Drawing::Point(154, 34);
+			this->txtUsuario->Name = L"txtUsuario";
+			this->txtUsuario->Size = System::Drawing::Size(205, 20);
+			this->txtUsuario->TabIndex = 2;
+			// 
+			// txtContraseña
+			// 
+			this->txtContraseña->Location = System::Drawing::Point(154, 67);
+			this->txtContraseña->Name = L"txtContraseña";
+			this->txtContraseña->Size = System::Drawing::Size(205, 20);
+			this->txtContraseña->TabIndex = 3;
+			// 
+			// btnIngresar
+			// 
+			this->btnIngresar->Location = System::Drawing::Point(154, 125);
+			this->btnIngresar->Name = L"btnIngresar";
+			this->btnIngresar->Size = System::Drawing::Size(158, 31);
+			this->btnIngresar->TabIndex = 4;
+			this->btnIngresar->Text = L"Ingresar";
+			this->btnIngresar->UseVisualStyleBackColor = true;
+			// 
+			// btnRegistrarse
+			// 
+			this->btnRegistrarse->Location = System::Drawing::Point(154, 209);
+			this->btnRegistrarse->Name = L"btnRegistrarse";
+			this->btnRegistrarse->Size = System::Drawing::Size(160, 25);
+			this->btnRegistrarse->TabIndex = 5;
+			this->btnRegistrarse->Text = L"Registrarse";
+			this->btnRegistrarse->UseVisualStyleBackColor = true;
+			this->btnRegistrarse->Click += gcnew System::EventHandler(this, &LoginForm::btnRegistrarse_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(167, 181);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(135, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"¿No tienes alguna cuenta\?";
+			// 
+			// LoginForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(458, 259);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->btnRegistrarse);
+			this->Controls->Add(this->btnIngresar);
+			this->Controls->Add(this->txtContraseña);
+			this->Controls->Add(this->txtUsuario);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"LoginForm";
+			this->Text = L"LoginForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	
+
+private: System::Void btnRegistrarse_Click(System::Object^ sender, System::EventArgs^ e) {
+	RegistroForm^ registroform = gcnew RegistroForm();
+	registroform->TopMost = true;
+	registroform->Show();
+
+}
+};
+}
---
+++ STPUCP_AdminGUIView/QuesoyForm.h
@@ -0,0 +1,90 @@
+#pragma once
+
+namespace STPUCPAdminGUIView {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de QuesoyForm
+	/// </summary>
+	public ref class QuesoyForm : public System::Windows::Forms::Form
+	{
+	public:
+		QuesoyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~QuesoyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Button^ button2;
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(102, 37);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(426, 125);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"ALUMNO";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(102, 221);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(426, 125);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"CHOFER";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// QuesoyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(625, 387);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"QuesoyForm";
+			this->Text = L"QuesoyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
